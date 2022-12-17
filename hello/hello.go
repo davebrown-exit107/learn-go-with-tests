94dda7fb64c97f3456dab00be3699fd0a533da6d
Dave Brown <dave.brown@exit107.com>
2022-12-17 12:44:15 -0700

    Add language parameter and Spanish prefix

--- hello/hello.go
+++ hello/hello.go
@@ -3,15 +3,24 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello"
+const spanishHelloPrefix = "Hola"
 
-func Hello(name string) string {
+func Hello(name, lang string) string {
+	var prefix string
 	if name == "" {
 		name = "world"
 	}
 
-	return fmt.Sprintf("%v, %v", englishHelloPrefix, name)
+	if lang == "EN" || lang == "" {
+		prefix = englishHelloPrefix
+	}
+	if lang == "ES" {
+		prefix = spanishHelloPrefix
+	}
+
+	return fmt.Sprintf("%v, %v", prefix, name)
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "EN"))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,18 +3,27 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("say hello with a parameter", func(t *testing.T) {
+	t.Run("say hello with a name parameter", func(t *testing.T) {
 
-		got := Hello("Chris")
+		got := Hello("Chris", "EN")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("say hello with no parameter", func(t *testing.T) {
+	t.Run("say hello with no name parameter", func(t *testing.T) {
 
-		got := Hello("")
+		got := Hello("", "EN")
 		want := "Hello, world"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("say hello in spanish with a name parameter", func(t *testing.T) {
+
+		got := Hello("Chris", "ES")
+		want := "Hola, Chris"
 
 		assertCorrectMessage(t, got, want)
 
