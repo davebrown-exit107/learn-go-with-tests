a8c11d5d71145a69381a951eccf86d9038878979
Dave Brown <dave.brown@exit107.com>
2022-12-17 12:34:46 -0700

    Refactor testing setup for DRY

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -8,9 +8,7 @@ func TestHello(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say hello with no parameter", func(t *testing.T) {
@@ -18,8 +16,14 @@ func TestHello(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
+
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
