51a3781da6de531b4fa6c27e2d4d8b5f592282fe
Dave Brown <dave.brown@exit107.com>
2022-12-17 12:52:02 -0700

    Refactor greeting into a separate function

--- hello/hello.go
+++ hello/hello.go
@@ -6,12 +6,7 @@ const englishHelloPrefix = "Hello"
 const spanishHelloPrefix = "Hola"
 const frenchHelloPrefix = "Bonjour"
 
-func Hello(name, lang string) string {
-	var prefix string
-	if name == "" {
-		name = "world"
-	}
-
+func greetingPrefix(lang string) (prefix string) {
 	switch lang {
 	case "EN":
 		prefix = englishHelloPrefix
@@ -21,8 +16,16 @@ func Hello(name, lang string) string {
 		prefix = frenchHelloPrefix
 	default:
 		prefix = englishHelloPrefix
-
 	}
+	return
+}
+
+func Hello(name, lang string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	prefix := greetingPrefix(lang)
 
 	return fmt.Sprintf("%v, %v", prefix, name)
 }
