5f9060c518aa2a82ab2728d7e91211e253b8adfe
Dave Brown <dave.brown@exit107.com>
2022-12-23 09:38:02 -0700

    Refactor tests into table driven tests

--- smi/smi_test.go
+++ smi/smi_test.go
@@ -6,50 +6,55 @@ import (
 )
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-
-		if got != want {
-			t.Errorf("wanted: %.2f got: %.2f", want, got)
-		}
+	tests := map[string]struct {
+		shape Shape
+		want  float64
+	}{
+		"rectangle": {
+			shape: Rectangle{10, 10},
+			want:  100.0,
+		},
+		"circle": {
+			shape: Circle{5},
+			want:  78.53981633974483,
+		},
 	}
 
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{W: 10, H: 10}
-		want := 100.0
-		checkArea(t, rectangle, want)
+	for name, curTest := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := curTest.shape.Area()
+			if got != curTest.want {
+				t.Errorf("wanted: %.2f got: %.2f", curTest.want, got)
+			}
+		})
+	}
 
-	})
-
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{R: 5}
-		want := 78.53981633974483
-		checkArea(t, circle, want)
-	})
 }
 
 func TestPerimeter(t *testing.T) {
-	checkPerimeter := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Perimeter()
-
-		if got != want {
-			t.Errorf("wanted: %.2f got: %.2f", want, got)
-		}
+	tests := map[string]struct {
+		shape Shape
+		want  float64
+	}{
+		"rectangle": {
+			shape: Rectangle{10, 10},
+			want:  40.0,
+		},
+		"circle": {
+			shape: Circle{5},
+			want:  31.41592653589793,
+		},
 	}
 
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{W: 10, H: 10}
-		want := 40.0
-		checkPerimeter(t, rectangle, want)
-	})
+	for name, curTest := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := curTest.shape.Perimeter()
+			if got != curTest.want {
+				t.Errorf("wanted: %.2f got: %.2f", curTest.want, got)
+			}
+		})
+	}
 
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{R: 5}
-		want := 31.41592653589793
-		checkPerimeter(t, circle, want)
-	})
 }
 
 func ExampleRectangle_Perimeter() {
