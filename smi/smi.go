0ed3bd6c906f376fdc41bc16cd54f12c25c506e7
Dave Brown <dave.brown@exit107.com>
2022-12-23 08:29:01 -0700

    Add Circle type with Area/Perimeter methods
    Refactor tests and add tests and examples for Circle type

--- smi/smi.go
+++ smi/smi.go
@@ -1,6 +1,8 @@
 // Testing structs, methods, and interfaces
 package smi
 
+import "math"
+
 // A type to hold Rectangle data and methods
 type Rectangle struct {
 	W float64 // Width of Rectangle
@@ -15,4 +17,19 @@ func (r *Rectangle) Perimeter() (perimeter float64) {
 // Calculate the area of a Rectangle
 func (r *Rectangle) Area() (area float64) {
 	return r.W * r.H
+}
+
+// A type to hold Circle data and methods
+type Circle struct {
+	R float64 // Radius
+}
+
+// Calculate the perimeter of a Rectangle
+func (c *Circle) Perimeter() (perimeter float64) {
+	return 2 * math.Pi * c.R
+}
+
+// Calculate the area of a Rectangle
+func (c *Circle) Area() (area float64) {
+	return math.Pi * math.Pow(c.R, 2)
 }
--- smi/smi_test.go
+++ smi/smi_test.go
@@ -5,24 +5,26 @@ import (
 	"testing"
 )
 
-func TestPerimeter(t *testing.T) {
-	testRectangle := Rectangle{W: 10, H: 10}
-	got := testRectangle.Perimeter()
-	want := 40.0
+func TestRectangle(t *testing.T) {
+	t.Run("perimeter", func(t *testing.T) {
+		testRectangle := Rectangle{W: 10, H: 10}
+		got := testRectangle.Perimeter()
+		want := 40.0
 
-	if got != want {
-		t.Errorf("wanted: %.2f got: %.2f", want, got)
-	}
-}
+		if got != want {
+			t.Errorf("wanted: %.2f got: %.2f", want, got)
+		}
+	})
 
-func TestArea(t *testing.T) {
-	testRectangle := Rectangle{W: 10, H: 10}
-	got := testRectangle.Area()
-	want := 100.0
+	t.Run("area", func(t *testing.T) {
+		testRectangle := Rectangle{W: 10, H: 10}
+		got := testRectangle.Area()
+		want := 100.0
 
-	if got != want {
-		t.Errorf("wanted: %.2f got: %.2f", want, got)
-	}
+		if got != want {
+			t.Errorf("wanted: %.2f got: %.2f", want, got)
+		}
+	})
 }
 
 func ExampleRectangle_Perimeter() {
@@ -37,4 +39,40 @@ func ExampleRectangle_Area() {
 	area := testRectangle.Area()
 	fmt.Println(area)
 	// Output: 100
+}
+
+func TestCircle(t *testing.T) {
+	t.Run("perimeter", func(t *testing.T) {
+		testCircle := Circle{R: 5}
+		got := testCircle.Perimeter()
+		want := 31.41592653589793
+
+		if got != want {
+			t.Errorf("wanted: %.2f got: %.2f", want, got)
+		}
+	})
+
+	t.Run("area", func(t *testing.T) {
+		testCircle := Circle{R: 5}
+		got := testCircle.Area()
+		want := 78.53981633974483
+
+		if got != want {
+			t.Errorf("wanted: %.2f got: %.2f", want, got)
+		}
+	})
+}
+
+func ExampleCircle_Perimeter() {
+	circle := Circle{R: 5}
+	perimeter := circle.Perimeter()
+	fmt.Println(perimeter)
+	// Output: 31.41592653589793
+}
+
+func ExampleCircle_Area() {
+	circle := Circle{R: 5}
+	area := circle.Area()
+	fmt.Println(area)
+	// Output: 78.53981633974483
 }
