1baffc872aacf64da27d1770ea2177755f1ca5ba
Dave Brown <dave.brown@exit107.com>
2023-02-24 11:16:23 -0700

    feat: Added Add() functionality to Dictionary

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -7,10 +7,15 @@ var ErrNotFound = errors.New("could not find the word you were looking for")
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
+
 	definition, ok := d[word]
 	if !ok {
 		return "", ErrNotFound
 	}
 
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
 }
--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -6,19 +6,39 @@ func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
 	t.Run("known word", func(t *testing.T) {
-		got, _ := dictionary.Search("test")
+		got, err := dictionary.Search("test")
 		want := "this is just a test"
 
+		assertNoError(t, err)
 		assertStrings(t, got, want)
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
+		got, err := dictionary.Search("unknown")
+		want := ""
 
 		assertError(t, err, ErrNotFound)
+		assertStrings(t, got, want)
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	assertNoError(t, err)
+	assertStrings(t, got, want)
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+
+	if got != nil {
+		t.Errorf("wanted no error, got %q", got)
+	}
+}
 func assertError(t testing.TB, got, want error) {
 	t.Helper()
 
