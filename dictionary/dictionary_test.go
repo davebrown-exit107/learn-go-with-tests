6cedc919104258f7d489af3c07a6385d8f1e4112
Dave Brown <dave.brown@exit107.com>
2023-02-25 13:07:13 -0700

    test: Add test coverage for non-existing word updates

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -47,14 +47,27 @@ func TestAdd(t *testing.T) {
 }
 
 func TestUpdate(t *testing.T) {
-	word := "test"
-	definition := "this is just a test"
-	dictionary := Dictionary{word: definition}
-	newDefinition := "new definition"
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefinition := "new definition"
 
-	dictionary.Update(word, newDefinition)
+		dictionary.Update(word, newDefinition)
 
-	assertDefinition(t, dictionary, word, newDefinition)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("non-existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newWord := "new definition"
+
+		err := dictionary.Update(newWord, definition)
+
+		assertError(t, err, ErrNotFound)
+	})
 }
 
 func assertError(t testing.TB, got, want error) {
