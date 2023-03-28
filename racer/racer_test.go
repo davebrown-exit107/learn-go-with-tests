ad310f231e65c429299cb3fece9da0c6170d656e
Dave Brown <dave.brown@exit107.com>
2023-03-27 18:16:58 -0600

    test(racer): Move tests to httptest server

--- racer/racer_test.go
+++ racer/racer_test.go
@@ -1,15 +1,33 @@
 package racer_test
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/davebrown-exit107/learn-go-with-tests/racer"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
-	got := Racer(slowURL, fastURL)
+	got := racer.Racer(slowURL, fastURL)
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+	slowServer.Close()
+	fastServer.Close()
 }
