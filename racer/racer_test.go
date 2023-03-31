0cdfdea47e8380e3bb1e3ee6307660c2ba48ea94
Dave Brown <dave.brown@exit107.com>
2023-03-31 11:12:35 -0600

    feat(racer): Add configurable timeout and ten second default

--- racer/racer_test.go
+++ racer/racer_test.go
@@ -10,21 +10,36 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+	t.Run("return faster server", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := racer.Racer(slowURL, fastURL)
+		want := fastURL
+		got, _ := racer.Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("return error on timeout", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := racer.ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Errorf("expected an error but did not get one")
+		}
+
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
