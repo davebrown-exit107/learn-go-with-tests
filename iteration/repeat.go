934e5291393bd19258b152204a5c7be9947c2bd9
Dave Brown <dave.brown@exit107.com>
2023-08-06 11:32:52 -0600

    Add numRepeat as a parameter and error handling to Repeat()
    Also added error handling to the tests and built out helper assert functions

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,12 +1,18 @@
 package iteration
 
-const numRepeat = 5
+import (
+	"fmt"
+)
 
-// Repeat a given string five times
-func Repeat(char string) string {
+// Repeat a given string n times
+func Repeat(char string, numRepeat int) (error, string) {
 	var repeated string
+	if numRepeat <= 0 {
+		err := fmt.Errorf("Got: %q. Expected a number greater than 0 for numRepeat.", numRepeat)
+		return err, ""
+	}
 	for i := 0; i < numRepeat; i++ {
 		repeated = repeated + char
 	}
-	return repeated
+	return nil, repeated
 }
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -8,23 +8,50 @@ import (
 )
 
 func TestRepeat(t *testing.T) {
-	got := iteration.Repeat("a")
-	want := "aaaaa"
+	t.Run("test string repetition", func(t *testing.T) {
+		err, got := iteration.Repeat("a", 10)
+		want := "aaaaaaaaaa"
 
-	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
-	}
+		assertNoError(t, err)
+		assertEqual(t, got, want)
+	})
+	t.Run("numRepeat less than zero", func(t *testing.T) {
+		err, got := iteration.Repeat("b", -5)
+		assertError(t, err)
+		assertEqual(t, got, "")
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		iteration.Repeat("a")
+		iteration.Repeat("a", 1)
 	}
 
 }
 
 func ExampleRepeat() {
-	repeated := iteration.Repeat("a")
+	_, repeated := iteration.Repeat("a", 5)
 	fmt.Println(repeated)
 	// Output: aaaaa
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Errorf("expected no error, got %q", err)
+	}
+}
+
+func assertEqual(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func assertError(t *testing.T, err error) {
+	t.Helper()
+	if err == nil {
+		t.Error("Expected an error and got nil")
+	}
 }
