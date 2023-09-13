4670679a45143d1a136d30265f91932ac958d98d
Dave Brown <dave.brown@exit107.com>
2023-09-13 17:14:02 -0600

    feat: Add perimeter function to calculate perimeter of rectangle

---
+++ smi/smi.go
@@ -0,0 +1,6 @@
+package smi
+
+// Perimeter takes two floats, one for each side, and returns the perimeter of the quadrangle
+func Perimeter(length, width float64) float64 {
+	return (length * 2) + (width * 2)
+}
---
+++ smi/smi_test.go
@@ -0,0 +1,17 @@
+package smi_test
+
+import (
+	"testing"
+
+	"github.com/davebrown-exit107/learn-go-with-tests/smi"
+)
+
+func TestPerimeter(t *testing.T) {
+	got := smi.Perimeter(10.0, 10.0)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f wanted %.2f", got, want)
+	}
+
+}
