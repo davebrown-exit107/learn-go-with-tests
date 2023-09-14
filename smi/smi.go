f57a11ac6d6208a1d4770a89de5812e6ce20f9f3
Dave Brown <dave.brown@exit107.com>
2023-09-13 18:18:36 -0600

    feat: add Circle type and methods to calculate the area

--- smi/smi.go
+++ smi/smi.go
@@ -1,17 +1,29 @@
 package smi
 
+import "math"
+
 // Rectangle represents a rectangle for calucations of area and perimeter
 type Rectangle struct {
 	Length float64
-	Width  float64
+	Height float64
 }
 
-// Perimeter takes two floats, one for each side, and returns the perimeter of the rectangle
-func Perimeter(r Rectangle) float64 {
-	return (r.Length * 2) + (r.Width * 2)
+// Calculate the perimeter of a rectangle
+func (r Rectangle) Perimeter() float64 {
+	return (r.Length * 2) + (r.Height * 2)
 }
 
-// Area takes two floats, length and width, and returns the area of the rectangle
-func Area(r Rectangle) float64 {
-	return r.Length * r.Width
+// Calculate the area of a rectangle
+func (r Rectangle) Area() float64 {
+	return r.Length * r.Height
+}
+
+// Circle represents a circle for calucations of area and perimeter
+type Circle struct {
+	Radius float64
+}
+
+// Calculate the area of a circle
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
 }
--- smi/smi_test.go
+++ smi/smi_test.go
@@ -7,22 +7,35 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	rect := smi.Rectangle{10.0, 10.0}
-	got := smi.Perimeter(rect)
-	want := 40.0
+	t.Run("rectangle", func(t *testing.T) {
+		rect := smi.Rectangle{10, 10}
+		got := rect.Perimeter()
+		want := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f wanted %.2f", got, want)
-	}
-
+		if got != want {
+			t.Errorf("got %.2f wanted %.2f", got, want)
+		}
+	})
 }
 
 func TestArea(t *testing.T) {
-	rect := smi.Rectangle{12.0, 6.0}
-	got := smi.Area(rect)
-	want := 72.0
+	t.Run("rectangle", func(t *testing.T) {
+		rect := smi.Rectangle{12, 6}
+		got := rect.Area()
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f wanted %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f wanted %.2f", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := smi.Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %.2f wanted %.2f", got, want)
+		}
+	})
 }
