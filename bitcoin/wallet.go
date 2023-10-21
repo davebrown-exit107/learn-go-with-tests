9226a3992a34b108b03a81a974b41a704150d180
Dave Brown <dave.brown@exit107.com>
2023-10-21 12:57:29 -0600

    feat: constant error definitions

--- bitcoin/wallet.go
+++ bitcoin/wallet.go
@@ -1,6 +1,19 @@
 package bitcoin
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type errorConst string
+
+const (
+	ErrOverdrawn     errorConst = "Account Overdrawn"
+	ErrInvalidAmount errorConst = "Invalid amount"
+)
+
+func (e errorConst) Error() string {
+	return string(e)
+}
 
 type Bitcoin int
 
@@ -12,15 +25,24 @@ func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
 
-func (w *Wallet) Deposit(amount Bitcoin) {
+func (w *Wallet) Deposit(amount Bitcoin) error {
 	if amount >= 0 {
 		w.balance += amount
+		return nil
+	} else {
+		return ErrInvalidAmount
 	}
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrOverdrawn
+	}
 	if amount >= 0 {
 		w.balance -= amount
+		return nil
+	} else {
+		return ErrInvalidAmount
 	}
 }
 
--- bitcoin/wallet_test.go
+++ bitcoin/wallet_test.go
@@ -8,6 +8,17 @@ import (
 )
 
 func TestWallet(t *testing.T) {
+	assertError := func(t testing.TB, got, want error) {
+		t.Helper()
+
+		if got == nil {
+			t.Errorf("wanted error %q, got nil", want)
+		}
+		if got != want {
+			t.Errorf("wanted error %q, got %q", want, got)
+		}
+	}
+
 	assertBalance := func(t testing.TB, wallet bitcoin.Wallet, want bitcoin.Bitcoin) {
 		t.Helper()
 		got := wallet.Balance()
@@ -17,30 +28,57 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
-	t.Run("deposit", func(t *testing.T) {
+	t.Run("deposit - normal", func(t *testing.T) {
 
 		wallet := bitcoin.Wallet{}
 
-		wallet.Deposit(bitcoin.Bitcoin(10))
+		_ = wallet.Deposit(bitcoin.Bitcoin(10))
 
 		want := bitcoin.Bitcoin(10)
 
 		assertBalance(t, wallet, want)
 	})
 
-	t.Run("withdraw", func(t *testing.T) {
+	t.Run("deposit - invalid amount", func(t *testing.T) {
 
 		wallet := bitcoin.Wallet{}
-		wallet.Deposit(20)
-		wallet.Withdraw(10)
+
+		got := wallet.Deposit(bitcoin.Bitcoin(-10))
+
+		assertError(t, got, bitcoin.ErrInvalidAmount)
+	})
+
+	t.Run("withdraw - normal", func(t *testing.T) {
+
+		wallet := bitcoin.Wallet{}
+		_ = wallet.Deposit(20)
+		_ = wallet.Withdraw(10)
 
 		want := bitcoin.Bitcoin(10)
 
 		assertBalance(t, wallet, want)
+	})
+
+	t.Run("withdraw - invalid amount", func(t *testing.T) {
+
+		wallet := bitcoin.Wallet{}
+		_ = wallet.Deposit(20)
+		got := wallet.Withdraw(-10)
+
+		assertError(t, got, bitcoin.ErrInvalidAmount)
+	})
+
+	t.Run("withdraw - overdrawn", func(t *testing.T) {
+
+		wallet := bitcoin.Wallet{}
+		_ = wallet.Deposit(20)
+		got := wallet.Withdraw(30)
+
+		assertError(t, got, bitcoin.ErrOverdrawn)
 	})
 	t.Run("stringer", func(t *testing.T) {
 		wallet := bitcoin.Wallet{}
-		wallet.Deposit(20)
+		_ = wallet.Deposit(20)
 
 		got := fmt.Sprintf("%s", wallet.Balance())
 		want := "20 BTC"
