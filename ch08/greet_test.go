44c00a1bd2cd8163a47ddd1c79991b7d1c3ed103
Dave Brown <dave.brown@exit107.com>
2023-12-03 16:55:24 -0700

    feat: build demo of dependency injection

---
+++ ch08/greet_test.go
@@ -0,0 +1,23 @@
+package di_test
+
+import (
+	"bytes"
+	"testing"
+
+	di "github.com/davebrown-exit107/learn-go-with-tests/ch08"
+)
+
+func TestGreet(t *testing.T) {
+	t.Run("test greeting", func(t *testing.T) {
+		buffer := bytes.Buffer{}
+		di.Greet(&buffer, "Chris")
+
+		got := buffer.String()
+		want := "Hello, Chris"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+
+	})
+}
