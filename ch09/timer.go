ea677000cc2d4e9af7a2e58e79e3d2a63703fdd2
Dave Brown <dave.brown@exit107.com>
2023-12-03 18:18:44 -0700

    feat: make the countdown dramatic

--- ch09/timer.go
+++ ch09/timer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const startingNumber = 3
@@ -12,6 +13,7 @@ const finalWord = "Go!"
 func Countdown(w io.Writer) {
 	for i := startingNumber; i > 0; i-- {
 		fmt.Fprintln(w, i)
+		time.Sleep(time.Second * 1)
 	}
 	fmt.Fprint(w, finalWord)
 }
