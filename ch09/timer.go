fc63a1f3d9e00ab2f0c32589d893af4abefd7456
Dave Brown <dave.brown@exit107.com>
2023-12-03 18:36:41 -0700

    test: mock timer to allow for faster tests

--- ch09/timer.go
+++ ch09/timer.go
@@ -10,14 +10,25 @@ import (
 const startingNumber = 3
 const finalWord = "Go!"
 
-func Countdown(w io.Writer) {
+type DefaultSleeper struct{}
+
+func (s *DefaultSleeper) Sleep(d time.Duration) {
+	time.Sleep(d)
+}
+
+type Sleeper interface {
+	Sleep(time.Duration)
+}
+
+func Countdown(w io.Writer, s Sleeper) {
 	for i := startingNumber; i > 0; i-- {
 		fmt.Fprintln(w, i)
-		time.Sleep(time.Second * 1)
+		s.Sleep(time.Second * 1)
 	}
 	fmt.Fprint(w, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
