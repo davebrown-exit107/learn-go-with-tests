149da1e58292c3c43af250d93361bc91812d9ea5
Dave Brown <dave.brown@exit107.com>
2023-12-03 19:22:30 -0700

    chore: import code for website checker
    Some changes were made to fit the house style.

---
+++ ch10/checker_test.go
@@ -0,0 +1,35 @@
+package concurrency_test
+
+import (
+	"reflect"
+	"testing"
+
+	concurrency "github.com/davebrown-exit107/learn-go-with-tests/ch10"
+)
+
+func mockWebsiteChecker(url string) bool {
+	if url == "waat://furhurterwe.geds" {
+		return false
+	}
+	return true
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	got := concurrency.CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("wanted %v, got %v", want, got)
+	}
+}
