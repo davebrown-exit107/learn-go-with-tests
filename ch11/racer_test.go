6e5682d3cf473fbe89caa4adbd221d2c7a0f415f
Dave Brown <dave.brown@exit107.com>
2023-12-05 23:49:01 -0700

    test: implement mock around testing Racer

--- ch11/racer_test.go
+++ ch11/racer_test.go
@@ -1,20 +1,34 @@
 package racer_test
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	racer "github.com/davebrown-exit107/learn-go-with-tests/ch11"
 )
 
 func TestRacer(t *testing.T) {
 	t.Run("compare two sites", func(t *testing.T) {
-		slowUrl := "http://www.facebook.com"
-		fastUrl := "http://www.quii.dev"
+		slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(20 * time.Millisecond)
+			w.WriteHeader(http.StatusOK)
+		}))
+		fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		slowUrl := slowServer.URL
+		fastUrl := fastServer.URL
 
 		want := fastUrl
 		got := racer.Racer(slowUrl, fastUrl)
 
 		assertEqualStr(t, got, want)
+
+		slowServer.Close()
+		fastServer.Close()
 	})
 
 }
