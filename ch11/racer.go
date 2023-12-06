ba4a8e7518ebe8f424c2f61003193bb7a05c9c92
Dave Brown <dave.brown@exit107.com>
2023-12-05 23:17:37 -0700

    feat: implement website racer function

--- ch11/racer.go
+++ ch11/racer.go
@@ -1,1 +1,23 @@
 package racer
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(urlA, urlB string) (winner string) {
+	startA := time.Now()
+	http.Get(urlA)
+	durationA := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(urlB)
+	durationB := time.Since(startB)
+
+	if durationA < durationB {
+		return urlA
+	} else {
+		return urlB
+	}
+
+}
--- ch11/racer_test.go
+++ ch11/racer_test.go
@@ -1,1 +1,27 @@
 package racer_test
+
+import (
+	"testing"
+
+	racer "github.com/davebrown-exit107/learn-go-with-tests/ch11"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("compare two sites", func(t *testing.T) {
+		slowUrl := "http://www.facebook.com"
+		fastUrl := "http://www.quii.dev"
+
+		want := fastUrl
+		got := racer.Racer(slowUrl, fastUrl)
+
+		assertEqualStr(t, got, want)
+	})
+
+}
+
+func assertEqualStr(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
