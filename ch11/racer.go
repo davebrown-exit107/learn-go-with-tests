cbd1ff86c46d408624cdb146f566aa88f8a568af
Dave Brown <dave.brown@exit107.com>
2023-12-06 01:14:54 -0700

    feat: add Configurable racer
    As this maintains the old Racer() with the default of 10 seconds, it mostly benefits us with faster tests.

--- ch11/racer.go
+++ ch11/racer.go
@@ -6,15 +6,20 @@ import (
 	"time"
 )
 
+const tenSecondTimeout = 10 * time.Second
+
 var ErrTimeOut = errors.New("both urls timed out")
 
 func Racer(urlA, urlB string) (winner string, err error) {
+	return ConfigurableRacer(urlA, urlB, tenSecondTimeout)
+}
+func ConfigurableRacer(urlA, urlB string, timeout time.Duration) (winner string, err error) {
 	select {
 	case <-ping(urlA):
 		return urlA, nil
 	case <-ping(urlB):
 		return urlB, nil
-	case <-time.After(10 * time.Second):
+	case <-time.After(timeout):
 		return "", ErrTimeOut
 	}
 }
