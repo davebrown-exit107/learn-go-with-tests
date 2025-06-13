4914adf8b06e7547de92f0c8fef5fef6df0ced40
Dave Brown <dave.brown@exit107.com>
2025-06-13 17:34:26 -0600

    tests: Refactor string comparison into a new function

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -6,22 +6,24 @@ import (
 	"github.com/davebrown-exit107/learn-go-with-tests/hello"
 )
 
+func assertStringsMatch(t *testing.T, want string, got string) {
+	t.Helper()
+	if want != got {
+		t.Errorf("wanted %q, got %q", want, got)
+	}
+}
 func TestHelloEn(t *testing.T) {
 	t.Run("say hello - with name", func(t *testing.T) {
 		got := hello.Hello("Dave", "EN")
 		want := "Hello, Dave"
 
-		if got != want {
-			t.Errorf("wanted %q got %q", want, got)
-		}
+		assertStringsMatch(t, want, got)
 	})
 	t.Run("say hello - no name", func(t *testing.T) {
 		got := hello.Hello("", "EN")
 		want := "Hello, world"
 
-		if got != want {
-			t.Errorf("wanted %q got %q", want, got)
-		}
+		assertStringsMatch(t, want, got)
 	})
 }
 
@@ -30,16 +32,12 @@ func TestHelloEs(t *testing.T) {
 		got := hello.Hello("Dave", "ES")
 		want := "Hola, Dave"
 
-		if got != want {
-			t.Errorf("wanted %q got %q", want, got)
-		}
+		assertStringsMatch(t, want, got)
 	})
 	t.Run("say hello - no name", func(t *testing.T) {
 		got := hello.Hello("", "ES")
 		want := "Hola, world"
 
-		if got != want {
-			t.Errorf("wanted %q got %q", want, got)
-		}
+		assertStringsMatch(t, want, got)
 	})
 }
