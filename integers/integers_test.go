fb6c33c009710b9eb50b0f81f979edb322da3d8f
Dave Brown <dave.brown@exit107.com>
2025-06-13 18:21:09 -0600

    feat: Add Add() function to integers package

--- integers/integers_test.go
+++ integers/integers_test.go
@@ -7,11 +7,30 @@ import (
 	"github.com/davebrown-exit107/learn-go-with-tests/integers"
 )
 
-func TestAdder(t *testing.T) {
-	got := integers.Add(2, 2)
-	want := 4
-
+func assertValuesMatch(t testing.TB, got int, want int) {
+	t.Helper()
 	if got != want {
-		fmt.Errorf("wanted %q, got %q", want, got)
+		fmt.Errorf("wanted %d, got %d", want, got)
 	}
+}
+
+func TestAdder(t *testing.T) {
+	t.Run("two positive numbers", func(t *testing.T) {
+		got := integers.Add(2, 2)
+		want := 4
+
+		assertValuesMatch(t, got, want)
+	})
+	t.Run("one negative one positive number", func(t *testing.T) {
+		got := integers.Add(-5, 10)
+		want := 5
+
+		assertValuesMatch(t, got, want)
+	})
+}
+
+func ExampleAdd() {
+	sum := integers.Add(2, 3)
+	fmt.Println(sum)
+	// Output: 5
 }
