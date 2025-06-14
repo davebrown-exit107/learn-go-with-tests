d673c5a1795886cec66d7082329f6a0a7565d201
Dave Brown <dave.brown@exit107.com>
2025-06-14 11:43:04 -0600

    perf(Repeat): move to strings.Builder for performance improvement

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,10 +1,12 @@
 package iteration
 
+import "strings"
+
 func Repeat(value string) string {
-	var rValue string
+	var rValue strings.Builder
 	count := 4
 	for i := 0; i < count; i++ {
-		rValue += value
+		rValue.WriteString(value)
 	}
-	return rValue
+	return rValue.String()
 }
