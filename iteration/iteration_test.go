352e401eaffa0a3dbe788544051b46eab2f83a6e
David Brown <dave.brown@exit107.com>
2025-06-14 16:36:40 -0600

    Ch3 pt2 (#12)
    * feat(Repeat): allow caller to specify number of times the string is repeated
    
    * docs(Repeat): add example of using Repeat()

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestRepeat(t *testing.T) {
 	t.Run("first test", func(t *testing.T) {
-		got := iteration.Repeat("a")
+		got := iteration.Repeat("a", 4)
 		want := "aaaa"
 
 		if got != want {
@@ -20,6 +20,14 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		iteration.Repeat("a")
+		iteration.Repeat("a", 4)
 	}
+}
+
+func ExampleRepeat() {
+	count := 4
+	value := "hello"
+	repeated := iteration.Repeat(value, count)
+	fmt.Println(repeated)
+	// Output: hellohellohellohello
 }
