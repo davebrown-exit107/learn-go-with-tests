efc75bbe286493735804282e6907dbe014c5b511
Dave Brown <dave.brown@exit107.com>
2025-07-21 17:40:26 -0600

    test(smi): refactor tests to the map pattern

--- smi/smi_test.go
+++ smi/smi_test.go
@@ -13,37 +13,46 @@ func assertEqual(t testing.TB, got, want float64) {
 	}
 }
 func TestPerimeter(t *testing.T) {
-	checkPerimeter := func(t testing.TB, shape smi.Shape, want float64) {
-		t.Helper()
-		got := shape.Perimeter()
-		assertEqual(t, got, want)
+	testCases := map[string]struct {
+		input smi.Shape
+		want  float64
+	}{
+		"rectangle": {
+			input: smi.Rectangle{10, 10},
+			want:  40.0,
+		},
+		"circle": {
+			input: smi.Circle{10.0},
+			want:  62.83185307179586,
+		},
 	}
-	t.Run("rectangle", func(t *testing.T) {
-		rect := smi.Rectangle{Height: 10.0, Width: 10.0}
-		want := 40.0
-		checkPerimeter(t, rect, want)
-	})
-	t.Run("circle", func(t *testing.T) {
-		circle := smi.Circle{Radius: 10.0}
-		want := 62.83185307179586
-		checkPerimeter(t, circle, want)
-	})
+	for name, test := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := test.input.Perimeter()
+			assertEqual(t, got, test.want)
+		})
+	}
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape smi.Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		assertEqual(t, got, want)
+	testCases := map[string]struct {
+		input smi.Shape
+		want  float64
+	}{
+		"rectangle": {
+			input: smi.Rectangle{10, 10},
+			want:  100.0,
+		},
+		"circle": {
+			input: smi.Circle{10.0},
+			want:  314.1592653589793,
+		},
 	}
-	t.Run("rectangle", func(t *testing.T) {
-		rect := smi.Rectangle{Height: 10.0, Width: 10.0}
-		want := 100.0
-		checkArea(t, rect, want)
-	})
-	t.Run("circle", func(t *testing.T) {
-		circle := smi.Circle{Radius: 10.0}
-		want := 314.1592653589793
-		checkArea(t, circle, want)
-	})
+
+	for name, test := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := test.input.Area()
+			assertEqual(t, got, test.want)
+		})
+	}
 }
