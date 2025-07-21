fe256ed88770105ee489b3707af4989fcb60ab2f
Dave Brown <dave.brown@exit107.com>
2025-07-21 16:12:34 -0600

    refactor(smi)!: move Perimeter() and Area() to methods under Rectangle

--- smi/smi_test.go
+++ smi/smi_test.go
@@ -7,21 +7,25 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	input := smi.Rectangle{Height: 10.0, Width: 10.0}
-	got := smi.Perimeter(input)
-	want := 40.0
+	t.Run("rectangle", func(t *testing.T) {
+		rect := smi.Rectangle{Height: 10.0, Width: 10.0}
+		got := rect.Perimeter()
+		want := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
 
 func TestArea(t *testing.T) {
-	input := smi.Rectangle{Height: 10.0, Width: 10.0}
-	got := smi.Area(input)
-	want := 100.0
+	t.Run("rectangle", func(t *testing.T) {
+		rect := smi.Rectangle{Height: 10.0, Width: 10.0}
+		got := rect.Area()
+		want := 100.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
