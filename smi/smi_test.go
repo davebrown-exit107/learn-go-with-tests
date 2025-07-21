7966a92fd8ef77092b1be5d6df38dcae20a01ccd
Dave Brown <dave.brown@exit107.com>
2025-07-21 16:43:28 -0600

    test(smi): refactor comparison to separate function

--- smi/smi_test.go
+++ smi/smi_test.go
@@ -6,24 +6,26 @@ import (
 	"github.com/davebrown-exit107/learn-go-with-tests/smi"
 )
 
+func assertEqual(t testing.TB, got, want float64) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
 func TestPerimeter(t *testing.T) {
 	t.Run("rectangle", func(t *testing.T) {
 		rect := smi.Rectangle{Height: 10.0, Width: 10.0}
 		got := rect.Perimeter()
 		want := 40.0
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		assertEqual(t, got, want)
 	})
 	t.Run("circle", func(t *testing.T) {
 		circle := smi.Circle{Radius: 10.0}
 		got := circle.Perimeter()
 		want := 62.83185307179586
 
-		if got != want {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertEqual(t, got, want)
 	})
 }
 
@@ -33,17 +35,13 @@ func TestArea(t *testing.T) {
 		got := rect.Area()
 		want := 100.0
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		assertEqual(t, got, want)
 	})
 	t.Run("circle", func(t *testing.T) {
 		circle := smi.Circle{Radius: 10.0}
 		got := circle.Area()
 		want := 314.1592653589793
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		assertEqual(t, got, want)
 	})
 }
