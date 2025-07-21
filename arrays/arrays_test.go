4a20f6018f6ca7b17a1da3647e268871a0db7599
Dave Brown <dave.brown@exit107.com>
2025-07-20 23:50:29 -0600

    test(arrays): remove redundant test

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -8,18 +8,7 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	t.Run("collection of five numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
-
-		got := arrays.Sum(numbers)
-		want := 15
-
-		if got != want {
-			t.Errorf("got %d want %d, given %v", got, want, numbers)
-		}
-	})
-
-	t.Run("collection of indeterminant size", func(t *testing.T) {
+	t.Run("collection of arbitrary size", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8}
 
 		got := arrays.Sum(numbers)
