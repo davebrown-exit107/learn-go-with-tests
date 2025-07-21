4a92515f549661bcb76b938963c19882076c37f9
Dave Brown <dave.brown@exit107.com>
2025-07-20 23:46:49 -0600

    test(arrays): move Sum() parameter numbers to slice instead of bounded array

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -1,19 +1,33 @@
 package arrays_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/davebrown-exit107/learn-go-with-tests/arrays"
 )
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of five numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := arrays.Sum(numbers)
-	want := 15
+		got := arrays.Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d, given %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of indeterminant size", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8}
+
+		got := arrays.Sum(numbers)
+		want := 36
+
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	})
 
 }
