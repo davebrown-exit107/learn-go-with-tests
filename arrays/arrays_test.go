7454a1c5cc4c9a2bf571ff8aaa3bf2f5f3ab204c
Dave Brown <dave.brown@exit107.com>
2025-07-21 00:22:23 -0600

    test(arrays): clean up redundant tests and add test for SumAll()

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -2,23 +2,33 @@ package arrays_test
 
 import (
 	"fmt"
+	"slices"
 	"testing"
 
 	"github.com/davebrown-exit107/learn-go-with-tests/arrays"
 )
 
 func TestSum(t *testing.T) {
-	t.Run("collection of arbitrary size", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8}
 
-		got := arrays.Sum(numbers)
-		want := 36
+	got := arrays.Sum(numbers)
+	want := 36
 
-		if got != want {
-			t.Errorf("got %d want %d, given %v", got, want, numbers)
-		}
-	})
+	if got != want {
+		t.Errorf("got %d want %d, given %v", got, want, numbers)
+	}
+}
 
+func TestSumAll(t *testing.T) {
+	numbersA := []int{1, 2, 3, 4, 5}
+	numbersB := []int{6, 7, 8, 9, 10}
+
+	got := arrays.SumAll(numbersA, numbersB)
+	want := []int{15, 40}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %d want %d", got, want)
+	}
 }
 
 func ExampleSum() {
